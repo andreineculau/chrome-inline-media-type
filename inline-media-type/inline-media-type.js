9336ed91f5c5595d6fe8aa2aa98a6b11e60ce8a4
bhenderson <bhenderson@yp.com>
2012-09-13 08:36:23 -0700

    Convert to using options page to change content type based off of url and content type matching.

--- inline-media-type/inline-media-type.js
+++ inline-media-type/inline-media-type.js
@@ -3,24 +3,41 @@
     "use strict";
 
     chrome.webRequest.onHeadersReceived.addListener(function(details) {
-        var mediaType,
-            RE = /(.*\+(json|xml))(;?.*)/;
+
+        var mediaType;
+
+        function matchHeader(header, url) {
+            var content_types = JSON.parse(localStorage['content_types']);
+            var success = false;
+
+            content_types.forEach(function(content_type) {
+                var url_re = new RegExp(content_type[0]);
+                var ct_re  = new RegExp(content_type[1]);
+                if (url_re.test(url) && ct_re.test(header)) {
+                  success = content_type[2];
+                }
+            });
+            return success;
+        }
+
 
         // Change Content-Type to "parent" media type
         // and add current content-type as a profile to the "parent" one
         details.responseHeaders.forEach(function(header) {
             var name = header.name.toLowerCase();
+            var new_type;
 
             if (name === 'content-type') {
-                if (RE.test(header.value)) {
+                if (new_type = matchHeader(header.value, details.url)) {
                     mediaType = true;
                     window.console.log('Changed Content-Type for request: ' + details.url);
                     window.console.log('from: ' + header.value);
-                    header.value = header.value.replace(RE, 'application/$2; profile=$1$3');
+                    header.value = new_type;
                     window.console.log('to: ' + header.value);
                 }
             }
         });
+
         // Display it inline
         details.responseHeaders.forEach(function(header) {
             var name = header.name.toLowerCase();
